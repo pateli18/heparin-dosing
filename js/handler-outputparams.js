8f6feb4b10756308f9c13770a3fa743f9b9592b9
Ihsaan Patel <ihsaan.patel@gmail.com>
2018-02-05 20:59:40 -0500

    added function to pass params from input page to dosing page

---
+++ js/handler-outputparams.js
@@ -0,0 +1,51 @@
+var patientParams = {'Weight':{type:'field', value:null},
+    'Height':{type:'field', value:null},
+    'Indication':{type:'field', value:null},
+    'Age':{type:'field', value:null},
+    'Creatinine':{type:'field', value:null},
+    'SOFA':{type:'field', value:null},
+    'Elixhauser':{type:'field', value:null},
+    'Gender':{type:'button', value:null},
+    'Ethnicity':{type:'button', value:null},
+    'Renal':{type:'button', value:null},
+    'PE':{type:'button', value:null},
+    'ICU':{type:'field', value:null}};
+
+function getParams() {
+    for (var param in patientParams) {
+
+        var unknownFlag;
+        if (param == 'Weight' || param == 'Height' || param == 'Indication') {
+            unknownFlag = false;
+        } else {
+            var paramClass = $('#na' + param).attr("class").split(' ');
+            unknownFlag = (paramClass[paramClass.length - 1] == 'na-clicked');
+        }
+
+        if (unknownFlag) {
+            patientParams[param].value = 'Unknown';
+        } else  if (patientParams[param].type == 'field') {
+            patientParams[param].value = $('#input' + param).val();
+        } else {
+            var button1Class = $('#input' + param + 1).attr("class").split(' ');
+            var button2Class = $('#input' + param + 2).attr("class").split(' ');
+            if (button1Class[button1Class.length - 2] == 'input-clicked') {
+                patientParams[param].value = $('#input' + param + 1).html();
+            } else if (button2Class[button2Class.length - 2] == 'input-clicked') {
+                patientParams[param].value = $('#input' + param + 2).html();
+            } else {
+                patientParams[param].value = 'Unknown';
+            }
+        }
+    }
+}
+
+function calculateDose() {
+    getParams();
+    var queryParams = {};
+    for (var param in patientParams) {
+        queryParams[param] = patientParams[param].value;
+    }
+    var query = $.param(queryParams);
+    window.location = '/prototype_v0/dosing_view.html?_ijt=bda6dajl46du6r2l46it5docj2&' + String(query);
+}
